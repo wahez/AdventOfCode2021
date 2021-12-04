ca80c5e8b937aa2100fc70c8c05b5b4f2f109184
Walther Zwart <walther.zwart@gmail.com>
2021-12-04 10:45:01 +0100

    q01b

--- main.cc
+++ main.cc
@@ -4,6 +4,7 @@
 
 
 int q01a(std::istream&);
+int q01b(std::istream&);
 
 
 int main(int argc, char** argv)
@@ -12,6 +13,8 @@ int main(int argc, char** argv)
 	auto runner = TestRunner(argc, argv);
 	runner.check(q01a, "01a", "q01.tst", 7);
 	runner.check(q01a, "01a", "q01.inp", 1292);
+	runner.check(q01b, "01b", "q01.tst", 5);
+	runner.check(q01b, "01b", "q01.inp", 1262);
 	const auto end = std::chrono::steady_clock::now();
 	using namespace std::literals;
 	std::cout << "Total : " << (end - start)/1ms << " ms" << std::endl;
--- src/q01.cc
+++ src/q01.cc
@@ -1,6 +1,25 @@
+#include <algorithm>
 #include <istream>
-#include <optional>
 #include <ranges>
+#include <vector>
+
+
+namespace
+{
+
+
+	void for_each_zipped(auto&& range1, auto&& range2, auto&& pred)
+	{
+		using std::begin;
+		using std::end;
+		auto c1 = begin(range1);
+		auto c2 = begin(range2);
+		for (; c1 != end(range1) && c2 != end(range2); ++c1, ++c2)
+			pred(*c1, *c2);
+	}
+
+
+}
 
 
 int q01a(std::istream& is)
@@ -8,16 +27,33 @@ int q01a(std::istream& is)
 	auto count = 0;
 	auto input = std::ranges::istream_view<int>(is);
 	auto current = input.begin();
-	if (current != input.end())
+	if (current == input.end())
+		throw std::runtime_error("Not enough values");
+	// go through the list without storing it (so no allocation)
+	auto previous = *current;
+	++current;
+	for (; current != input.end(); ++current)
 	{
-		auto previous = *current;
-		++current;
-		for (; current != input.end(); ++current)
-		{
-			if (*current > previous)
-				++count;
-			previous = *current;
-		}
+		if (*current > previous)
+			++count;
+		previous = *current;
 	}
+	return count;
+}
+
+
+int q01b(std::istream& is)
+{
+	auto numbers = std::vector<int>{};
+	std::ranges::move(
+			std::ranges::istream_view<int>(is),
+			std::back_inserter(numbers));
+	if (numbers.size() < 4)
+		throw std::runtime_error("Not enough values");
+	// only check if the number added to the sliding window is bigger than the number
+	auto numbers_offset = numbers | std::views::drop(3);
+	auto count = 0;
+	// zip_view is not in std yet
+	for_each_zipped(numbers, numbers_offset, [&](const auto& l, const auto& r) { if (r > l) ++count; });
 	return count;
 }
