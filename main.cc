7a1708952e2817f9ca1d0d8741225d4400560e8c
Walther Zwart <walther.zwart@gmail.com>
2021-12-04 10:32:51 +0100

    Added first version of main

---
+++ main.cc
@@ -0,0 +1,17 @@
+#include "test_runner.h"
+#include <chrono>
+#include <istream>
+
+
+int q01a(std::istream&);
+
+
+int main(int argc, char** argv)
+{
+	const auto start = std::chrono::steady_clock::now();
+	auto runner = TestRunner(argc, argv);
+	// runner.check(q01a, "01a", "q01.tst", 0);
+	const auto end = std::chrono::steady_clock::now();
+	using namespace std::literals;
+	std::cout << "Total : " << (end - start)/1ms << " ms" << std::endl;
+}
---
+++ test_runner.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <chrono>
+#include <filesystem>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <set>
+#include <string>
+#include <string_view>
+
+
+class TestRunner
+{
+public:
+	explicit TestRunner(int argc, char** argv)
+	{
+		for (int i = 1; i < argc; ++i)
+			tests_to_run.insert(argv[i]);
+	}
+
+	void solve(auto&& func, std::string_view name, const std::filesystem::path& path)
+	{
+		if (!tests_to_run.empty() && !tests_to_run.contains(name))
+		{
+			std::cout << "Test " << name << " skipped" << std::endl;
+			return;
+		}
+		const auto result = run(func, name, path);
+		std::cout << "Test " << name << " produced " << result << std::endl;
+	}
+
+	void check(auto&& func, std::string_view name, const std::filesystem::path& path, const auto& expected_output)
+	{
+		if (!tests_to_run.empty() && !tests_to_run.contains(name))
+		{
+			std::cout << "Test " << name << " skipped" << std::endl;
+			return;
+		}
+		const auto result = run(func, name, path);
+		if (result != expected_output)
+			std::cerr << "Test " << name << " failed with input (" << path << "), expected: " << expected_output << ", got: " << result << std::endl;
+	}
+
+private:
+	static auto run(auto&& func, std::string_view name, const std::filesystem::path& path)
+	{
+		std::ifstream is("input" / path);
+		if (!is.good())
+			throw std::runtime_error("Could not find " + path.string());
+		const auto start = std::chrono::steady_clock::now();
+		const auto result = func(is);
+		const auto end = std::chrono::steady_clock::now();
+		using namespace std::literals;
+		std::cout << "Test " << name << " ran in " << std::setw(6) << (end - start)/1us << " us with input " << path << std::endl;
+		return result;
+	}
+	std::set<std::string, std::less<>> tests_to_run;
+};
