ae5dd203c91a130b2275847856b8751e21f3d027
Walther Zwart <walther.zwart@gmail.com>
2021-12-17 19:33:52 +0100

    q16b

--- src/q16.cc
+++ src/q16.cc
@@ -1,6 +1,5 @@
 #include <algorithm>
 #include <istream>
-#include <optional>
 #include <ranges>
 #include <vector>
 #include <cassert>
@@ -10,27 +9,12 @@
 namespace
 {
 
-	class Bitset
+
+	auto shift_in(auto value, auto new_bits, auto num_bits)
 	{
-	public:
-		constexpr explicit Bitset() : bits(0), bits_used(0) {}
-		constexpr explicit Bitset(std::uint8_t b) : bits(b), bits_used(4) {}
-
-		constexpr std::int8_t size() const { return bits_used; }
-
-		constexpr std::uint8_t extract(int num_bits)
-		{
-			assert(num_bits <= bits_used);
-			const auto shifted = bits >> (bits_used-num_bits);
-			const auto mask = (1<<num_bits)-1;
-			bits_used -= num_bits;
-			return shifted & mask;
-		}
-
-	private:
-		std::uint8_t bits = 0;
-		std::int8_t bits_used = 0;
-	};
+		const auto mask = (1<<num_bits)-1;
+		return (value << num_bits) | (new_bits & mask);
+	}
 
 
 	class BitReader
@@ -40,52 +24,47 @@ namespace
 
 		auto get_bits_consumed() const { return bits_consumed; }
 
-		template<typename T>
-		void consume(int num_bits, T& target)
+		template<typename T=int>
+		T consume(int num_bits)
 		{
 			assert(sizeof(T)*8 >= static_cast<std::size_t>(num_bits));
 			bits_consumed += num_bits;
-			auto value = 0ull;
-			auto read_bits = [&](const auto bits_to_read)
-			{
-				value = (value << bits_to_read) | cached_bits.extract(bits_to_read);
-				num_bits -= bits_to_read;
-			};
-			read_bits(std::min<int>(num_bits, cached_bits.size()));
-			while (num_bits >= 4)
-			{
-				ensure_bits_present();
-				read_bits(4);
-			}
-			ensure_bits_present();
-			read_bits(num_bits);
-			target = static_cast<T>(value);
-		}
-
-	private:
-		std::istream& is;
-		Bitset cached_bits;
-		int bits_consumed = 0;
-
-		void ensure_bits_present()
-		{
-			if (cached_bits.size() == 0)
+			for (; cached_bits_in_use < num_bits; cached_bits_in_use += 4)
 			{
 				const auto c = is.get();
 				if (!is.good() || c == EOF)
 					throw std::runtime_error("EOF");
 				else if (c > '9')
-					cached_bits = Bitset(c - 'A' + 10);
+					cached_bits = shift_in(cached_bits, c - 'A' + 10, 4);
 				else
-					cached_bits = Bitset(c - '0');
+					cached_bits = shift_in(cached_bits, c - '0', 4);
 			}
+			const auto mask = (1<<num_bits)-1;
+			cached_bits_in_use -= num_bits;
+			return (cached_bits >> cached_bits_in_use) & mask;
 		}
+
+		template<typename T>
+		void consume(int num_bits, T& target) { target = static_cast<T>(consume<int>(num_bits)); }
+
+	private:
+		std::istream& is;
+		std::uint64_t cached_bits = 0;
+		std::int8_t cached_bits_in_use = 0;
+		int bits_consumed = 0;
 	};
 
 
 	enum class TypeID : std::int8_t
 	{
+		SUM = 0,
+		PRODUCT = 1,
+		MINIMUM = 2,
+		MAXIMUM = 3,
 		LITERAL = 4,
+		GREATER = 5,
+		LESS = 6,
+		EQUAL = 7,
 	};
 
 
@@ -100,24 +79,8 @@ namespace
 	{
 		std::int8_t version;
 		TypeID type_id;
-		LengthTypeID length_type_id;
 	};
 
-
-	struct Packet
-	{
-		PacketHeader header;
-		std::optional<std::int64_t> number;
-		std::vector<Packet> sub_packets;
-
-		void print(std::ostream& os, int indent = 0) const
-		{
-			os << std::string(indent, ' ') << NotChar(header.version) << "|" << static_cast<int>(header.type_id) << "|" << static_cast<int>(header.length_type_id) << '\n';
-			os << std::string(indent, ' ') << sub_packets.size() << '\n';
-			for (const auto& sp: sub_packets)
-				sp.print(os, indent+2);
-		}
-	};
 
 	auto read_literal(BitReader& bit_reader)
 	{
@@ -126,91 +89,46 @@ namespace
 		do
 		{
 			bit_reader.consume(1, continue_bit);
-			auto next_bits = 0;
-			bit_reader.consume(4, next_bits);
-			value <<= 4;
-			value |= next_bits;
+			auto next_bits = bit_reader.consume(4);
+			value = shift_in(value, next_bits, 4);
 		}
 		while (continue_bit);
 		return value;
 	}
 
-	auto read(BitReader& bit_reader, auto&& func)
+	auto parse_packets(BitReader& bit_reader, auto&& process_packet)
 	{
-		using ResultType = decltype(func(PacketHeader{}, 0));
+		using ResultType = decltype(process_packet(PacketHeader{}, 0));
 		auto header = PacketHeader{};
 		bit_reader.consume(3, header.version);
 		bit_reader.consume(3, header.type_id);
 		if (header.type_id == TypeID::LITERAL)
-			return func(header, read_literal(bit_reader));
+			return process_packet(header, read_literal(bit_reader));
 		else
 		{
 			auto sub_results = std::vector<ResultType>{};
-			bit_reader.consume(1, header.length_type_id);
-			switch (header.length_type_id)
+			auto length_type_id = LengthTypeID{};
+			bit_reader.consume(1, length_type_id);
+			switch (length_type_id)
 			{
 			case LengthTypeID::NUM_BITS:
 			{
-				auto num_bits = 0;
-				bit_reader.consume(15, num_bits);
+				const auto num_bits = bit_reader.consume(15);
 				const auto current_bits_consumed = bit_reader.get_bits_consumed();
 				while (bit_reader.get_bits_consumed() < current_bits_consumed + num_bits)
-					sub_results.push_back(read(bit_reader, func));
+					sub_results.push_back(parse_packets(bit_reader, process_packet));
 				break;
 			}
 			case LengthTypeID::NUM_PACKETS:
 			{
-				auto num_sub_packets = 0;
-				bit_reader.consume(11, num_sub_packets);
+				const auto num_sub_packets = bit_reader.consume(11);
 				for (auto i = 0; i < num_sub_packets; ++i)
-					sub_results.push_back(read(bit_reader, func));
+					sub_results.push_back(parse_packets(bit_reader, process_packet));
 				break;
 			}
 			}
-			return func(header, std::move(sub_results));
+			return process_packet(header, std::move(sub_results));
 		}
-	}
-
-
-	int sum_versions(const Packet& packet)
-	{
-		return accumulate(packet.sub_packets | std::views::transform(sum_versions), static_cast<int>(packet.header.version));
-	}
-
-
-	std::istream& operator>>(std::istream& is, Packet& packet)
-	{
-		auto bit_reader = BitReader(is);
-		if (is.good() && is.peek() != '\n' && is.peek() != EOF)
-		{
-			packet = read(bit_reader, [](const PacketHeader& header, auto sub) -> Packet
-			{
-				auto result = Packet{header};
-				if constexpr(std::is_integral_v<decltype(sub)>)
-					result.number = sub;
-				else
-					result.sub_packets = std::move(sub);
-				return result;
-			});
-			while (is.peek() == '0')
-				is.get();
-			is >> Assert("\n");
-		}
-		else
-			is.get();
-		return is;
-	}
-
-
-	int read_sum_version(BitReader& bit_reader)
-	{
-		return read(bit_reader, [](const PacketHeader& header, auto sub) -> int
-		{
-			if constexpr(std::is_integral_v<decltype(sub)>)
-				return header.version;
-			else
-				return accumulate(sub, static_cast<int>(header.version));
-		});
 	}
 
 
@@ -219,13 +137,41 @@ namespace
 
 int q16a(std::istream& is)
 {
-	return accumulate(
-		std::ranges::istream_view<Packet>(is) | std::views::transform(sum_versions), 0);
+	auto bit_reader = BitReader(is);
+	return parse_packets(bit_reader, [&]<typename PacketData>(const PacketHeader& header, PacketData data) -> int
+	{
+		if constexpr(std::is_integral_v<PacketData>)
+			return header.version;
+		else
+			return accumulate(data, static_cast<int>(header.version));
+	});
 }
 
 
-int q16b(std::istream& is)
+std::int64_t q16b(std::istream& is)
 {
 	auto bit_reader = BitReader(is);
-	return read_sum_version(bit_reader);
+	return parse_packets(bit_reader, [&]<typename PacketData>(const PacketHeader& header, PacketData data) -> std::int64_t
+	{
+		if constexpr(std::is_integral_v<PacketData>)
+		{
+			assert(header.type_id == TypeID::LITERAL);
+			return data;
+		}
+		else
+		{
+			switch (header.type_id)
+			{
+			case TypeID::SUM    : return accumulate(data, 0ll, std::plus<>{});
+			case TypeID::PRODUCT: return accumulate(data, 1ll, std::multiplies<>{});
+			case TypeID::MINIMUM: return *std::ranges::min_element(data);
+			case TypeID::MAXIMUM: return *std::ranges::max_element(data);
+			case TypeID::LITERAL: throw std::runtime_error("LITERAL with sub packets");
+			case TypeID::GREATER: assert(data.size() == 2); return data[0] > data[1];
+			case TypeID::LESS   : assert(data.size() == 2); return data[0] < data[1];
+			case TypeID::EQUAL  : assert(data.size() == 2); return data[0] == data[1];
+			default: throw std::runtime_error("Unknown TypeID");
+			}
+		}
+	});
 }
